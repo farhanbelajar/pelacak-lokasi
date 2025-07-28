e0f273c1f4c67804c25aa1526804c077c14205f1
farhan <hanmcs0804@gmail.com>
2025-07-29 04:03:30 +0700

    hai update ye

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Ganti dengan data dari Firebase Console
 const firebaseConfig = {
-  apiKey: "AIzaSyXXX",
-  authDomain: "pelacak-lokasi.firebaseapp.com",
-  databaseURL: "https://pelacak-lokasi-default-rtdb.firebaseio.com",
-  projectId: "pelacak-lokasi",
-  storageBucket: "pelacak-lokasi.appspot.com",
+  apiKey: "AIzaSyAzjdU6NpuVTUWbOzFGbVCfq3LK_YOS58g",
+  authDomain: "absen-b6048.firebaseapp.com",
+  databaseURL: "https://absen-b6048-default-rtdb.firebaseio.com/",
+  projectId: "absen-b6048",
+  storageBucket: "absen-b6048.appspot.com",
   messagingSenderId: "1234567890",
   appId: "1:1234567890:web:abcd1234"
 };
